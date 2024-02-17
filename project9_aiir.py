71c79b6c982bf2ab6820eb31d5f7393eb08f70ff
Yego99 <154710402+Yego99@users.noreply.github.com>
2024-02-17 12:22:35 -0600

    Add files via upload

---
+++ project9_aiir.py
@@ -0,0 +1,16 @@
+from imageai.Classification import ImageClassification
+import os 
+
+execution_path = os.getcwd()
+
+prediction = ImageClassification()
+prediction.setModelTypeAsMobileNetV2() #This sets the model we want to use a few are already preloaded 
+prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2-b0353104.pth"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "house.jpg"), result_count=3 )  #Result count is how many predictions we want the model to give us
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(f'{eachPrediction} , : , {eachProbability}')
+
+
+#For this project we used 
